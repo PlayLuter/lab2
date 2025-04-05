c7f99ab9d0dd5a312ab36720401df41256ce1acf
PlayLuter <89547747+PlayLuter@users.noreply.github.com>
2025-04-05 22:29:36 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  build: {
+    minify: true,
+  },
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.tsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      tsconfigRaw: {
+        compilerOptions: {
+          skipLibCheck: true,
+          noImplicitAny: false, // Это отключит ошибки неявного any
+        },
+      },
+    },
+  },
 })
